2d6c9d6fb0645f445e398f37c51992c94fa3630c
Abdul Hassan <Abdul Hassan@localhost>
2011-04-23 20:20:35 -0400

    Fixed bug for homepage login form (missing name attribute for submit button). Made error messages more dynamic, read from config settings/constants.

--- src/application/messages/models/reservation.php
+++ src/application/messages/models/reservation.php
@@ -2,11 +2,19 @@
 
 return array(
 	'start_time' => array(
-		'min_time_before_start' => 'Reservation must be made at least 30 minutes in advance',
-		'max_time_before_start' => 'Reservations can only be made for 12 weeks in the future',
+		'min_time_before_start' => 'Reservation must be made at least '
+		                        .  Date::span(0, Model_Reservation::CURRENT_TIME_START_TIME_GAP, 'minutes')
+		                        .  ' minutes in advance',
+		'max_time_before_start' => 'Reservations can only be made for '
+		                        .  Date::span(0, Model_Reservation::CURRENT_TIME_START_TIME_MAX_GAP, 'weeks')
+		                        .  ' weeks in the future',
 	),
 	'end_time' => array(
-		'min_reservation_length' => 'Reservations must be atleast 30 minutes long',
-		'min_time_before_end'    => 'Reservation can only be edited 30 minutes prior to ending',
+		'min_reservation_length' => 'Reservations must be atleast '
+		                         .  Date::span(0, Model_Reservation::START_TIME_END_TIME_GAP, 'minutes')
+		                         .  ' minutes long',
+		'min_time_before_end' => 'Reservation can only be edited '
+		                      .  Date::span(0, Model_Reservation::CURRENT_TIME_END_TIME_GAP, 'minutes')
+		                      .  ' minutes prior to ending',
 	),
 );
--- src/application/messages/models/reservation/_external.php
+++ src/application/messages/models/reservation/_external.php
@@ -2,10 +2,14 @@
 
 return array(
 	'start_time' => array(
-		'min_time_before_cancel' => 'Reservations can only be cancelled 30 minutes before they begin',
+		'min_time_before_cancel' => 'Reservations can only be cancelled '
+		                         .  Date::span(0, Model_Reservation::CURRENT_TIME_START_TIME_GAP, 'minutes')
+		                         .  ' minutes before they begin',
 	),
 	'end_recurrence' => array(
 		'not_empty'             => 'You must provide an end date for your recurrence',
-		'max_time_before_start' => 'Your end date for recurrence is too far in the future. 12 week max'
+		'max_time_before_start' => 'Your end date for recurrence is too far in the future. '
+		                        .  Date::span(0, Model_Reservation::CURRENT_TIME_START_TIME_MAX_GAP, 'weeks')
+		                        . ' week max'
 	),
 );
