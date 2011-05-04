c7999c39a2f095897d65d0ce4ad99472910c3bb4
Abdul Hassan <Abdul Hassan@localhost>
2011-05-04 05:11:31 -0400

    Added basic info to the home page.

--- src/application/classes/view/home/index.php
+++ src/application/classes/view/home/index.php
@@ -11,4 +11,16 @@
 class View_Home_Index extends View_Base
 {
 	public $title = 'Home';
+
+	public function price_plan()
+	{
+		return Model_PricePlan::active_price_plan()->as_array();
+	}
+
+	public function open_spots()
+	{
+		return ORM::factory('garage')
+			->where('open', '=', TRUE)
+			->count_all();
+	}
 } // End View_Home_Index
